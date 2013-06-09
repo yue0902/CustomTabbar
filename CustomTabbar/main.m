cabeb724bec07beaec6955a4f732b0171397cc64
l <l@ltekiMac.local>
2013-06-09 09:44:22 +0800

    Initial Commit

---
+++ CustomTabbar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CustomTabbar
+//
+//  Created by l on 13-6-9.
+//  Copyright (c) 2013年 l. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
